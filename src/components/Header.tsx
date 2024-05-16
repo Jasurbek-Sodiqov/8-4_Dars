cfcba9e24ab1017b2bd7cce780a5c4ef15c44210
Jasurbek-Sodiqov <jasurbeksodiqov20021223@gmail.com>
2024-05-17 02:26:21 +0500

    8-4_Dars

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,22 +56,6 @@ const Header: FC<PrTaype> = (props) => {
 
   return (
     <div className="flex max-w-[1440px] justify-between mx-auto items-center gap-24 pt-8 border-b-2 border-blue-500">
-      <div className="flex-1">
-        <label className="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-      </div>
       <button
         onClick={() =>
           (document.getElementById("my_modal_1") as HTMLFormElement).showModal()
